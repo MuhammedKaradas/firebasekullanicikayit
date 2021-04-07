9021793a7f41f556ef3f57f949f1c6a40a0ac680
Muhammed KARADAŞ <31390903+MuhammedKaradas@users.noreply.github.com>
2021-04-08 00:05:29 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+//import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'kayitekrani.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //Firebase'i Çalıştır
+  //await Firebase.initializeApp();
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: KayitUygulama(),
+  ));
+}
+
+class KayitUygulama extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return KayitEkrani();
+  }
+}
